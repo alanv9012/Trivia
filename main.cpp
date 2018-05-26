c6b1dfe09dc1766efcc169d39d9a294dcf2f5d9f
alanv9012 <39658122+alanv9012@users.noreply.github.com>
2018-05-26 21:28:29 +0000

    Add files via upload
    Aqui esta el main, si quieren agreguen aqui lo que llevamos y de aqui lo empezamos a modificar.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+/**
+ * Funcion que actualiza la puntuacion solamente se manda imprimir, recibe como parametro la puntuacion
+ * @param score
+ */
+
+void actualizarPuntuacion(int score){
+
+   
+
+};
+
+int main() {
+    
+    return 0;
+}
